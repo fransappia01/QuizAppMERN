2366976f3345fbde0d592e38f134ab372d330f74
fransappia01 <fransappia01@gmail.com>
2024-03-02 01:20:08 -0300

    g

--- questions-app/src/api/record.js
+++ questions-app/src/api/record.js
@@ -1,48 +1,26 @@
-const backendURL = 'https://quiz-app-mern-back.vercel.app';
-
 const headers = new Headers();
 headers.append('Content-Type', 'application/json');
 headers.append('Access-Control-Allow-Origin', '*');
 
 const getRecords = async () => {
-  try {
-    const res = await fetch(`${backendURL}/api/allresults`, {
-      method: 'GET',
-      headers: headers,
-    });
-
-    if (!res.ok) {
-      throw new Error('Error al obtener los registros');
-    }
-
-    const records = await res.json();
-    console.log(records, "recordianoo");
-    return records;
-  } catch (error) {
-    console.error('Error en la función getRecords:', error);
-    throw error;
-  }
+  const res = await fetch(`https://quiz-app-mern-back.vercel.app/api/allresults`, {
+    method: 'GET',
+    headers: headers,
+  });
+  const records = await res.json();
+  console.log(records,"recordianoo");
+  return records;
 };
 
 const postRecord = async ({ nombre, puntuacion }) => {
-  try {
-    const res = await fetch(`${backendURL}/api/result`, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({ nombre, puntuacion }),
-    });
-
-    if (!res.ok) {
-      throw new Error('Error al enviar el registro');
-    }
-
-    const record = await res.json();
-    console.log(record);
-    return record;
-  } catch (error) {
-    console.error('Error en la función postRecord:', error);
-    throw error;
-  }
+  const res = await fetch(`https://quiz-app-mern-back.vercel.app/api/result`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ nombre, puntuacion }),
+  });
+  const record = await res.json();
+  console.log(record);
+  return record;
 };
 
 export {
